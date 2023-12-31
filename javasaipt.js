d381a75072429741017edf23f4d98a59bdef29d9
Muthukumar_K <mail2muthukumar007@gmail.com>
2023-12-31 09:06:05 +0530

    javascript.js

--- javasaipt.js
+++ javasaipt.js
@@ -70,7 +70,7 @@ var number = document.getElementsByClassName("number");
 for(var i =0;i<number.length;i++){
     number[i].addEventListener('click',function(){
         var output=reverseNumberFormat(getOutput());
-        if(output!=NaN){ //if output is a number
+        if(output!=NaN){
             output=output+this.id;
             printOutput(output);
         }
